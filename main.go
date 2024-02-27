c4fd6c672c8e7f3c4042d95ff492acbb589cdc01
Editor <dutti.coder@gmail.com>
2024-02-28 01:25:59 +0500

    В main.go добавлены основные обработчики для струтуры "User"

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
+
+	router := gin.Default()
+
+	router.POST("/register", func(context *gin.Context) {
+
+	})
+
+	router.DELETE("/user/:id", func(context *gin.Context) {
+
+	})
+
+	router.PATCH("/user/:id", func(context *gin.Context) {
+
+	})
 
 }
