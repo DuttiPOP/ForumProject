e3db57d247d96f02f2411c4e4e117b0d934327c0
Editor <dutti.coder@gmail.com>
2024-02-28 03:49:54 +0500

    Добавлен конструктор для БД

--- model/DB.go
+++ model/DB.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
 type DatabaseConfig struct {
 	Host     string `yaml:"db.host"`
 	Port     string `yaml:"db.port"`
@@ -7,4 +12,19 @@ type DatabaseConfig struct {
 	Password string `yaml:"db.password"`
 	DBName   string `yaml:"db.dbname"`
 	SSLMode  string `yaml:"db.sslmode"`
+}
+
+func NewDataBase(cfg DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
