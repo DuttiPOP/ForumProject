f4b3d7606000d784cb93de162ba4533222a0387b
Editor <dutti.coder@gmail.com>
2024-02-28 03:53:26 +0500

    Добавлен интерфейс IUserService в пакет для сервисов

--- model/service/service.go
+++ model/service/service.go
@@ -1,12 +1,20 @@
 package service
 
 import (
+	"ForumProject/model/entity"
 	"ForumProject/model/repository"
 )
 
+type IUserService interface {
+	Create(user entity.User) (int, error)
+	Get(id int) (user entity.User, err error)
+	Delete(id int) error
+}
+
 type Service struct {
+	IUserService
 }
 
 func NewService(repository repository.Repository) *Service {
-	return &Service{}
+	return &Service{NewUserService(repository.IUserRepository)}
 }
