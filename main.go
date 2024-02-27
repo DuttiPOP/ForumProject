b0fa1aa0da586ef8bde9f8ea79e27a3d2aea5b4c
Editor <dutti.coder@gmail.com>
2024-02-28 03:58:48 +0500

    Добавлено подключение к БД

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"ForumProject/model"
+	"github.com/gin-gonic/gin"
 	"gopkg.in/yaml.v3"
+	"log"
 	"os"
+)
+
 const (
 	config_path = "model/config.yaml"
 )
@@ -10,6 +15,14 @@ const (
 func main() {
 
 	router := gin.Default()
+
+	var dbconfig *model.DatabaseConfig = loadConfig()
+	db, err := model.NewDataBase(*dbconfig)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
 
 	router.POST("/register", func(context *gin.Context) {
 
