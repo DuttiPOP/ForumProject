ae1af382e00391580399ca35ac8de99209c480e1
Editor <dutti.coder@gmail.com>
2024-02-28 03:40:46 +0500

    Добавлен интерфейс IUserRepository в пакет для репозиторий

--- model/repository/repository.go
+++ model/repository/repository.go
@@ -1,12 +1,26 @@
 package repository
 
 import (
+	"ForumProject/model/entity"
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	userTable = "users"
+)
+
+type IUserRepository interface {
+	Create(user entity.User) (int, error)
+	Get(id int) (user entity.User, err error)
+	Delete(id int) error
+}
+
 type Repository struct {
+	IUserRepository
 }
 
 func NewRepository(db *sqlx.DB) *Repository {
-	return &Repository{}
+	return &Repository{
+		NewUserRepository(db),
+	}
 }
