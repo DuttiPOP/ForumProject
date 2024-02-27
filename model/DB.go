71fcac100f615610db8d887ec8a27d7efd82d572
Editor <dutti.coder@gmail.com>
2024-02-28 03:49:29 +0500

    Добавлен тэг `yaml` в структуру Конфига базы данных

--- model/DB.go
+++ model/DB.go
@@ -1,10 +1,10 @@
 package model
 
 type DatabaseConfig struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	DBName   string
-	SSLMode  string
+	Host     string `yaml:"db.host"`
+	Port     string `yaml:"db.port"`
+	Username string `yaml:"db.username"`
+	Password string `yaml:"db.password"`
+	DBName   string `yaml:"db.dbname"`
+	SSLMode  string `yaml:"db.sslmode"`
 }
