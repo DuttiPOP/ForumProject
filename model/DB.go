bf673860e50912bdf420f5ada8538e8a5ea783d0
Editor <dutti.coder@gmail.com>
2024-02-28 07:40:03 +0500

    Подключение к БД переписано с jmoiron/sqlx на gorm

--- model/DB.go
+++ model/DB.go
@@ -2,7 +2,8 @@ package model
 
 import (
 	"fmt"
-	"github.com/jmoiron/sqlx"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type DatabaseConfig struct {
@@ -14,17 +15,9 @@ type DatabaseConfig struct {
 	SSLMode  string `yaml:"sslmode"`
 }
 
-func NewDataBase(cfg *DatabaseConfig) (*sqlx.DB, error) {
-	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
-	if err != nil {
-		return nil, err
-	}
+func NewDataBase(cfg *DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s TimeZone=Asia/Shanghai",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
