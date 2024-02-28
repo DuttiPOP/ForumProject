0555ae2b2314da54535d517c5e707177d904215a
Editor <dutti.coder@gmail.com>
2024-02-28 07:47:42 +0500

    сущность User переписана под GORM

--- model/entity/user.go
+++ model/entity/user.go
@@ -1,8 +1,10 @@
 package entity
 
+import "gorm.io/gorm"
+
 type User struct {
-	ID       int    `json:"id" db:"id"`
-	Username string `json:"username" db:"username"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"password,omitempty" db:"password"`
+	gorm.Model
+	Username string `gorm:"type:varchar(255);not null;unique" json:"username" db:"username"`
+	Email    string `gorm:"type:varchar(255);not null;unique" json:"email" db:"email"`
+	Password string `gorm:"type:varchar(255);not null" json:"password,omitempty" db:"password"`
 }
