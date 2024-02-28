7f6295d4dc659bdd7254c6ad3e6ec64f87723e84
Editor <dutti.coder@gmail.com>
2024-02-28 06:31:54 +0500

    Добавлены функции валидации атрибутов структуры User

--- model/service/UserService.go
+++ model/service/UserService.go
@@ -3,6 +3,27 @@ package service
 import (
 	"ForumProject/model/entity"
 	"ForumProject/model/repository"
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+const (
+	minUsernameLen = 3
+	maxUsernameLen = 20
+	minPasswordLen = 6
+	maxPasswordLen = 100
+)
+
+var (
+	ErrEmailRequired     = errors.New("field \"email\" is required")
+	ErrInvalidEmail      = errors.New("field \"email\" is invalid")
+	ErrUsernameRequired  = errors.New(fmt.Sprintf("field \"username\" is required and must be between %d and %d characters", minUsernameLen, maxUsernameLen))
+	ErrPasswordRequired  = errors.New(fmt.Sprintf("field \"password\" is required and must be between %d and %d characters", minPasswordLen, maxPasswordLen))
+	ErrEmailDuplicate    = errors.New("email is already taken")
+	ErrUsernameDuplicate = errors.New("username is already taken")
+	ErrInvalidUserId     = errors.New("invalid user id")
 )
 
 type UserService struct {
@@ -17,10 +38,23 @@ func (service *UserService) Create(user entity.User) (int, error) {
 	return service.repository.Create(user)
 }
 
-func (service *UserService) Get(id int) (user entity.User, err error) {
-	return service.repository.Get(id)
+func ValidateUserFields(user *entity.User) error {
+	if user.Email == "" {
+		return ErrEmailRequired
+	}
+	if !isValidEmail(user.Email) {
+		return ErrInvalidEmail
+	}
+	if len(user.Username) < minUsernameLen || len(user.Username) > maxUsernameLen {
+		return ErrUsernameRequired
+	}
+	if len(user.Password) < minPasswordLen || len(user.Password) > maxPasswordLen {
+		return ErrPasswordRequired
+	}
+	return nil
 }
 
-func (service *UserService) Delete(id int) error {
-	return service.repository.Delete(id)
+func isValidEmail(email string) bool {
+	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return re.MatchString(email)
 }
