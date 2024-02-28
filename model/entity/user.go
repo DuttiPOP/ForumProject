bc49d809b88e4d511075a40752803bc6b69585c5
Editor <dutti.coder@gmail.com>
2024-02-28 06:28:53 +0500

    Переименовал поле name в username

--- model/entity/user.go
+++ model/entity/user.go
@@ -2,7 +2,7 @@ package entity
 
 type User struct {
 	ID       int    `json:"id" db:"id"`
-	Username string `json:"name" db:"name"`
+	Username string `json:"username" db:"username"`
 	Email    string `json:"email" db:"email"`
 	Password string `json:"password,omitempty" db:"password"`
 }
